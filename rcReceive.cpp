29b8d075d028a6ea63a62395f5c5ca1b60273ae5
Emil Ström <emil.strm@gmail.com>
2014-05-20 15:22:24 +0200

    Fix

--- rcReceive.cpp
+++ rcReceive.cpp
@@ -7,18 +7,28 @@ void setup() {
 	mySwitch.enableReceive(0);
 }
 
-int characterI, messageLength;
+int characterI = 0, messageLength = 0;
 char []messageBuffer;
 
 void loop() {
 	if (mySwitch.available()) {
 		int value = mySwitch.getReceivedValue();
 
-		if (characterRead <= 0) {
+		if (characterI <= 0) {
 			messageBuffer[messageLength - characterI] = (char)value;
-			if (--characterRead <= 0) Serial.println(messageBuffer);
+			Serial.print("Character ");
+			Serial.print(messageLength - characterI);
+			Serial.print(" is ");
+			Serial.println((char)value);
+
+			if (--characterRead <= 0) {
+				messageBuffer[messageLength] = '/0';
+				Serial.println(messageBuffer);
+			}
 		} else {
-			messageBuffer = new char[value];
+			messageBuffer = new char[value+1];
+			messageLength = value;
+			characterI = value;
 			Serial.print("Receiving new message, length: ");
 			Serial.println(value);
 		}
